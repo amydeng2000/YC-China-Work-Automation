e456114a02d0417cd95369750b26cb43fb3f7236
Amy Deng <kexindeng2000@gmail.com>
2020-11-03 18:44:44 -0800

    changed ocr file and added screenshots

--- webcrawling&ImageToText/ocr.py
+++ webcrawling&ImageToText/ocr.py
@@ -6,12 +6,12 @@ import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
-img = cv2.imread('./emails/5c2eb51c530c7008bbb9953c.png', cv2.IMREAD_UNCHANGED)
+path = r'C:\Users\12246\Desktop\YC-China-Work-Automation\webcrawling&ImageToText\emails\001-53f31a52dabfae9a84430cc8.png'
+img = cv2.imread(path)
 
-# img = Image.open('./emails/5c2eb51c530c7008bbb9953c.png')
-# img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY)
+cv2.imshow('image', img) 
+ret, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 # img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-
 
 scale_percent = 200 # percent of original size
 width = int(img.shape[1] * scale_percent / 100)
@@ -20,8 +20,5 @@ dim = (width, height)
 # resize image
 resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
-
-ret,img = cv2.threshold(np.array(img), 0, 255, cv2.THRESH_BINARY)
-
-text = pytesseract.image_to_string(img, lang="eng", timeout=5)
+text = pytesseract.image_to_string(img, lang="eng", timeout=15)
 print(text)
